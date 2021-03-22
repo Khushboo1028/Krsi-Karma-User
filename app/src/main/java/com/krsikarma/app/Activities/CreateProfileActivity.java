03b46dd6fccfcbf8b037c3acf395af79d4808ade
Khushboo <khushboo1028@gmail.com>
2021-03-22 22:07:52 +0530

    Bugs fixed

--- app/src/main/java/com/krsikarma/app/Activities/CreateProfileActivity.java
+++ app/src/main/java/com/krsikarma/app/Activities/CreateProfileActivity.java
@@ -100,9 +100,6 @@ public class CreateProfileActivity extends AppCompatActivity {
     Utils utils;
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -110,8 +107,6 @@ public class CreateProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_profile);
 
         init();
-
-
 
 
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -122,20 +117,14 @@ public class CreateProfileActivity extends AppCompatActivity {
                 first_name = utils.capitalizeString(et_first_name.getText().toString().trim());
                 last_name = utils.capitalizeString(et_last_name.getText().toString().trim());
 
-                if(!utils.isStringNull(first_name) && !utils.isStringNull(last_name) && !first_name.isEmpty() && !last_name.isEmpty()){
+                if (!utils.isStringNull(first_name) && !utils.isStringNull(last_name) && !first_name.isEmpty() && !last_name.isEmpty()) {
                     progressBar.setVisibility(View.VISIBLE);
-                    if(utils.isInternetAvailable(CreateProfileActivity.this)){
-                        addData();
-                    }else{
-                        btn_register.setEnabled(true);
-                    }
-
-                }else{
+                    addData();
+                } else {
                     progressBar.setVisibility(View.GONE);
                     btn_register.setEnabled(true);
-                    utils.alertDialogOK(CreateProfileActivity.this,getString(R.string.error),getString(R.string.complete_all_fields));
+                    utils.alertDialogOK(CreateProfileActivity.this, getString(R.string.error), getString(R.string.complete_all_fields));
                 }
-
 
 
             }
@@ -150,11 +139,9 @@ public class CreateProfileActivity extends AppCompatActivity {
         });
 
 
-
-
     }
 
-    private void init(){
+    private void init() {
 
         img_profile = (ImageView) findViewById(R.id.img_profile);
         et_first_name = (EditText) findViewById(R.id.et_first_name);
@@ -164,12 +151,11 @@ public class CreateProfileActivity extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
 
 
-
         utils = new Utils();
 
 
         phone_number = getIntent().getStringExtra("phone_number");
-        if(phone_number!=null){
+        if (phone_number != null) {
             tv_phone.setText(phone_number.substring(3));
         }
 
@@ -179,21 +165,21 @@ public class CreateProfileActivity extends AppCompatActivity {
 
         db = FirebaseFirestore.getInstance();
         firebaseStorage = FirebaseStorage.getInstance();
-        storageReference=firebaseStorage.getReference();
+        storageReference = firebaseStorage.getReference();
         mAuth = FirebaseAuth.getInstance();
-        firebaseUser=mAuth.getCurrentUser();
+        firebaseUser = mAuth.getCurrentUser();
     }
 
-    private void addData(){
+    private void addData() {
         progressBar.setVisibility(View.VISIBLE);
-        if(bitmapImage!=null){
-            final StorageReference ref = storageReference.child("profile_images/"+ UUID.randomUUID().toString());
+        if (bitmapImage != null) {
+            final StorageReference ref = storageReference.child("profile_images/" + UUID.randomUUID().toString());
 
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
             bitmapImage.compress(Bitmap.CompressFormat.PNG, 50, byteArrayOutputStream); // bmp is bitmap from user image file
             bitmapImage.recycle();
             byte[] byteArray = byteArrayOutputStream.toByteArray();
-            uploadTask=ref.putBytes(byteArray);
+            uploadTask = ref.putBytes(byteArray);
 
             uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                 @Override
@@ -209,15 +195,15 @@ public class CreateProfileActivity extends AppCompatActivity {
             }).addOnCompleteListener(new OnCompleteListener() {
                 @Override
                 public void onComplete(@NonNull Task task) {
-                    if(task.isSuccessful()){
+                    if (task.isSuccessful()) {
 
-                        String profile_image_url= task.getResult().toString();
+                        String profile_image_url = task.getResult().toString();
 
 
                         //adding data to firestore
                         Map<String, Object> data = new HashMap<>();
                         data.put(getString(R.string.user_id), firebaseUser.getUid());
-                        data.put(getString(R.string.first_name),first_name);
+                        data.put(getString(R.string.first_name), first_name);
                         data.put(getString(R.string.last_name), last_name);
                         data.put(getString(R.string.phone_number), phone_number);
                         data.put(getString(R.string.profile_img_url), profile_image_url);
@@ -231,7 +217,7 @@ public class CreateProfileActivity extends AppCompatActivity {
                                         progressBar.setVisibility(View.GONE);
                                         Log.d(TAG, "DocumentSnapshot successfully written!");
                                         finishAffinity();
-                                        Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                                         startActivity(intent);
                                     }
                                 })
@@ -244,16 +230,16 @@ public class CreateProfileActivity extends AppCompatActivity {
                                 });
 
 
-                    }else{
-                        Log.i(TAG,"An Error Occurred in uploading image" +task.getException().getMessage());
+                    } else {
+                        Log.i(TAG, "An Error Occurred in uploading image" + task.getException().getMessage());
                     }
                 }
             });
-        }else{
+        } else {
             //adding data to firestore
             Map<String, Object> data = new HashMap<>();
             data.put(getString(R.string.user_id), firebaseUser.getUid());
-            data.put(getString(R.string.first_name),first_name);
+            data.put(getString(R.string.first_name), first_name);
             data.put(getString(R.string.last_name), last_name);
             data.put(getString(R.string.phone_number), phone_number);
             data.put(getString(R.string.date_created), new Date());
@@ -267,7 +253,7 @@ public class CreateProfileActivity extends AppCompatActivity {
                             Log.d(TAG, "DocumentSnapshot successfully written!");
                             progressBar.setVisibility(View.GONE);
                             finishAffinity();
-                            Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                             startActivity(intent);
                         }
                     })
@@ -281,52 +267,40 @@ public class CreateProfileActivity extends AppCompatActivity {
         }
     }
 
-    private void setProfilePicture(){
+    private void setProfilePicture() {
 
         final String takePhoto = getString(R.string.take_photo);
         final String chooseFromLibrary = getString(R.string.choose_from_gallery);
 
 
-       final CharSequence[] items = {takePhoto, chooseFromLibrary};
-       android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
-       builder.setItems(items, new DialogInterface.OnClickListener() {
-           @Override
-           public void onClick(DialogInterface dialog, int item) {
+        final CharSequence[] items = {takePhoto, chooseFromLibrary};
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
 
 
-               if (items[item].equals(chooseFromLibrary)) {
+                if (items[item].equals(chooseFromLibrary)) {
 
-                       Intent intent = new Intent();
-                       intent.setType("image/*");
-                       intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                       intent.setAction(Intent.ACTION_GET_CONTENT);
-                       startActivityForResult(Intent.createChooser(intent,""), RESULT_LOAD_IMAGE_MULTIPLE);
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, ""), RESULT_LOAD_IMAGE_MULTIPLE);
 
-               }
-               else if (items[item].equals(takePhoto)) {
+                } else if (items[item].equals(takePhoto)) {
 
-
-
-                       Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                       try {
-                           file_camera_uri =  FileProvider.getUriForFile(getApplicationContext(), getPackageName() + ".provider", createImageFile());
-                       } catch (IOException e) {
-                           e.printStackTrace();
-                       }
-                       intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                       intent.putExtra(MediaStore.EXTRA_OUTPUT,file_camera_uri );
-                       startActivityForResult(intent, RESULT_CAMERA);
-                   }
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, RESULT_CAMERA);
+                }
 
 
-
-           }
-       });
-       builder.show();
-
+            }
+        });
+        builder.show();
 
 
-   }
+    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -344,22 +318,23 @@ public class CreateProfileActivity extends AppCompatActivity {
         mCurrentPhotoPath = "file:" + image.getAbsolutePath();
         return image;
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RESULT_LOAD_IMAGE_MULTIPLE && resultCode == Activity.RESULT_OK) {
 
-                if(data.getData()!=null) {
+            if (data.getData() != null) {
 
-                    Uri mImageUri = data.getData();
+                Uri mImageUri = data.getData();
 
-                    try {
-                        bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImageUri);
-                        img_profile.setImageBitmap(bitmapImage);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                try {
+                    bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImageUri);
+                    img_profile.setImageBitmap(bitmapImage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
 
             }
@@ -367,21 +342,10 @@ public class CreateProfileActivity extends AppCompatActivity {
 
         }
 
-        if (requestCode == RESULT_CAMERA && resultCode == Activity.RESULT_OK) {
-
-            Log.i(TAG,"Here");
-            if(file_camera_uri!=null) {
-                Log.i(TAG, "Here 2");
-                try {
-                    bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), file_camera_uri);
-                    img_profile.setImageBitmap(bitmapImage);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-
+        if (requestCode == RESULT_CAMERA && resultCode == RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            bitmapImage = (Bitmap) extras.get("data");
+            img_profile.setImageBitmap(bitmapImage);
         }
     }
 
@@ -393,7 +357,8 @@ public class CreateProfileActivity extends AppCompatActivity {
                         Manifest.permission.READ_EXTERNAL_STORAGE,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE
                 ).withListener(new MultiplePermissionsListener() {
-            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
 
                 if (report.areAllPermissionsGranted()) {
                     setProfilePicture();
@@ -401,7 +366,7 @@ public class CreateProfileActivity extends AppCompatActivity {
 
                 // check for permanent denial of any permission
                 if (report.isAnyPermissionPermanentlyDenied()) {
-                    Log.i(TAG,"Permissions permanently denied. Open Settings");
+                    Log.i(TAG, "Permissions permanently denied. Open Settings");
 
 
                     AlertDialog.Builder builder;
@@ -425,7 +390,9 @@ public class CreateProfileActivity extends AppCompatActivity {
                 }
 
             }
-            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
                 token.continuePermissionRequest();
             }
 
@@ -448,9 +415,9 @@ public class CreateProfileActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        if(listenerRegistration!=null){
+        if (listenerRegistration != null) {
             listenerRegistration.remove();
-            listenerRegistration=null;
+            listenerRegistration = null;
         }
     }
 }
